4faee3f882619a08bc1c7f0b187e69748292a170
maxpsurcov <max.psurcov1@gmail.com>
2022-10-04 17:39:47 +0300

    second commit (its 3rd lab)

--- lr2_opp_202tn/Bitstring.cpp
+++ lr2_opp_202tn/Bitstring.cpp
@@ -1,181 +1,67 @@
 #include "Bitstring.h"
 #include <iostream>
 using namespace std;
-
-//Bitstring Bitstring::Init(unsigned long one, unsigned long two)
-//{
-//	Bitstring tmp;
-//	tmp.one = one;
-//	tmp.two = two;
-//
-//	return tmp;
-//}
-
-
-void Bitstring::Display()
-{
-
-}
-
 char Bitstring::toString()
 {
-
-	return 0;
+    return 0;
 }
-
-
-Bitstring::~Bitstring()
+Bitstring::Bitstring()
 {
-	unit = 0;
-	count = 0;
+    unit = 0;
+    count = 0;
 }
-
-void Bitstring::Read()         //функция для ввода с клавиатуры
+Bitstring::Bitstring(unsigned long *one, unsigned long two)
 {
-	string str;
-	cout << "Введите битовую строку (пример: 10011) " << endl;
-	cin >> str;
-
-	count = str.size();
-
-	for (unsigned long i = 0; i < count; i++)          // проверка
-		if ((str.at(i) != *"0") && (str.at(i) != *"1"))
-			throw Error();                         //генерация исключения
-
-	unit = new unsigned long;
-
-	for (unsigned long i = 0; i < count; i++)        // присваеваем в массив нашу строку
-		unit[i] = str.at(i);
+    unit = one;
+    count = two;
 }
-
-void Bitstring::Display()            //функция для вывода на екран
+void Bitstring::Read()         //пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 {
-	for (unsigned long i = 0; i < count; i++)
-		cout << unit[i];
+    string str;
+    cout << "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ (пїЅпїЅпїЅпїЅпїЅпїЅ: 10011) " << endl;
+    cin >> str;
+    count = str.size();
+    for (unsigned long i = 0; i < count; i++)          // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+        if ((str.at(i) != *"0") && (str.at(i) != *"1"))
+            throw Error();                         //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+    unit = new unsigned long;
+    for (unsigned long i = 0; i < count; i++)        // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+        unit[i] = str.at(i);
 }
-
+void Bitstring::Display()            //пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+{
+    for (unsigned long i = 0; i < count; i++)
+        cout << unit[i];
+}
 int Bitstring::SizeOfBitstring()
 {
-	return count;
+    return this->count;
 }
-
-string Bitstring::NOT()      //опирация отрицание
-{
-    string str;
-
-    for (unsigned int i = 0; i < count; i++)
-    {
-        if (unit[i] == *"0")
-            unit[i] = *"1";
-        else
-            if (unit[i] == *"1")
-                unit[i] = *"0";
-
-        str += unit[i];
-    }
-    return str;
-}
-
-
-string Bitstring::NOT(unsigned long)      //перегрузка опирации отрицание
-{
-    string str;
-
-    for (unsigned int i = 0; i < count; i++)
-    {
-        if (unit[i] == *"0")
-            unit[i] = *"1";
-        else
-            if (unit[i] == *"1")
-                unit[i] = *"0";
-
-        str += unit[i];
-    }
-    return str;
-}
-
-string Bitstring::AND(Bitstring& bit)    //операция перемножить
-{
-    string str;
-
-    for (unsigned int i = 0; i < count; i++)
-    {
-        if ((unit[i] == *"0") && (bit.unit[i] == *"1"))
-            str += *"0";
-        else
-            if ((unit[i] == *"1") && (bit.unit[i] == *"0"))
-                str += *"0";
-            else
-                str += unit[i];
-    }
-    return str;
-}
-
-
-string Bitstring::OR(Bitstring& bit)   //операция сложить
-{
-    string str;
-
-    for (unsigned int i = 0; i < count; i++)
-    {
-        if ((unit[i] == *"0") && (bit.unit[i] == *"1"))
-            str += *"1";
-        else
-            if ((unit[i] == *"1") && (bit.unit[i] == *"0"))
-                str += *"1";
-            else
-                str += unit[i];
-    }
-    return str;
-}
-
-string Bitstring::XOR(Bitstring& bit)    //операция сложить по модулю
-{
-    string str;
-
-    for (unsigned int i = 0; i < count; i++)
-    {
-        if ((unit[i] == *"0") && (bit.unit[i] == *"1"))
-            str += *"1";
-        else
-            if ((unit[i] == *"1") && (bit.unit[i] == *"0"))
-                str += *"1";
-            else
-                if ((unit[i] == *"0") && (bit.unit[i] == *"0"))
-                    str += *"0";
-                else
-                    if ((unit[i] == *"1") && (bit.unit[i] == *"1"))
-                        str += *"0";
-    }
-    return str;
-}
-
-
-string Bitstring::rShift(unsigned long n)      //операция сдвиг битов на n елементов вправо
-{
-	string str;
-
-	for (unsigned long i = 0; i < count; i++)
-		str += unit[i];
-
-	str.erase(0, n);							 // удаляем n символов начитая с первого
-
-	for (int i = 0; i < n; i++)                    // добавляем n нулей в конец
-		str.append("0");
-
-	return str;
-}
-
-string Bitstring::lShift(unsigned long n)  //операция сдвиг битов на n елементов влево
-{
-	string str;
-
-	for (unsigned long i = 0; i < count; i++)
-		str += unit[i];
-
-	str.erase(count - n, n);  // удаляем n символов начитая с первого                   
-
-	str.insert(0, n, *"0");           // добавляем n нулей в  начало
-
-	return str;
+string Bitstring::rShift(unsigned long n)      //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ n пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ 
+{ 
+ string str; 
+ 
+ for (unsigned long i = 0; i < count; i++) 
+  str += unit[i]; 
+ 
+ str.erase(0, n);        // пїЅпїЅпїЅпїЅпїЅпїЅпїЅ n пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ 
+ 
+ for (int i = 0; i < n; i++)                    // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ n пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅ 
+  str.append("0"); 
+ 
+ return str; 
+} 
+ 
+string Bitstring::lShift(unsigned long n)  //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ n пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ 
+{ 
+ string str; 
+ 
+ for (unsigned long i = 0; i < count; i++) 
+  str += unit[i]; 
+ 
+ str.erase(count - n, n);  // пїЅпїЅпїЅпїЅпїЅпїЅпїЅ n пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ                    
+ 
+ str.insert(0, n, *"0");           // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ n пїЅпїЅпїЅпїЅпїЅ пїЅ  пїЅпїЅпїЅпїЅпїЅпїЅ 
+ 
+ return str; 
 }
